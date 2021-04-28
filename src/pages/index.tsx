49a52adea1602d6db51d78af3943ac4642ea5808
hstrada <h.strada@hotmail.com>
2021-04-28 08:03:03 -0300

    create service api with axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,41 @@
-export default function Home(props) {
+import { GetStaticProps } from 'next';
+import { api } from '../services/api';
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  published_at: string;
+  thumbnail: string;
+  description: string;
+  file: {
+    url: string;
+    type: string;
+    duration: number;
+  };
+};
+
+type HomeProps = {
+  episodes: Array<Episode>;
+};
+
+export default function Home(props: HomeProps) {
   return (
     <div>
-      {' '}
       <h1>hello</h1>
       <p>{JSON.stringify(props.episodes)}</p>
     </div>
   );
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes');
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('/episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    },
+  });
 
   return {
     props: {
@@ -18,4 +43,4 @@ export async function getStaticProps() {
     },
     revalidate: 60 * 60 * 8,
   };
-}
+};
