76d8e373a476f14960c72c3ce8d218f4db5e7c47
hstrada <h.strada@hotmail.com>
2021-04-28 08:19:38 -0300

    fix episode type

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,15 +7,13 @@ import { convertDurationToTimeString } from '../utils/convertDurationToTimeStrin
 type Episode = {
   id: string;
   title: string;
-  members: string;
-  published_at: string;
   thumbnail: string;
   description: string;
-  file: {
-    url: string;
-    type: string;
-    duration: number;
-  };
+  members: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publisheAt: Date;
 };
 
 type HomeProps = {
